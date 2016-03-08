91c1fd1562c844f4f1094427566ce69febbba6ec
Jarek <jarek@MacBook-Pro-Jarek-2.local>
2016-03-08 15:53:12 +0100

    sample_projekt

---
+++ iOS/features/environment.py
@@ -0,0 +1,26 @@
+from lib.appium_starter import start_appium, stop_appium
+# from lib.create_driver import create_driver
+from lib.create_driver import create_driver
+
+def before_all(context):
+    start_appium()
+
+def before_feature(context, feature):
+    create_driver('iOS', reinstallApp=True)
+    print("Before feature\n")
+
+#Scenario level objects are popped off context when scenario exits
+def before_scenario(context,scenario):
+    print("Before scenario\n")
+
+def after_scenario(context,scenario):
+    context.driver.quit()
+    print("After scenario\n")
+
+def after_feature(context,feature):
+    print("\nAfter feature")
+
+def after_all(context):
+    print("Executing after all")
+    stop_appium()
+
